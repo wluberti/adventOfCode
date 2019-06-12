0020441c4fcf135ff21e88edbc15adf7d02ad704
Wouter Luberti <wluberti@Wouters-MacBook-Pro.local>
2019-06-12 21:11:01 +0200

    Made it readable

--- 3/2.py
+++ 3/2.py
@@ -5,21 +5,28 @@ import re
 with open("input.txt", "r") as f:
     data = f.read().split("\n")
 
-SIZE = 1000
+SIZE = 1001
 canvas = []
 
 for i in range(SIZE):
     canvas.append([[]] * SIZE)    
 
 for line in data:
-    m = re.match(r"#(?P<id>\d+) @ (?P<xpos>\d+),(?P<ypos>\d+): (?P<width>\d+)x(?P<height>\d+)", line)
+    m = re.match(r"#(?P<id>\d+) @ (?P<xpos>\d+),(?P<ypos>\d+): (?P<width>\d+)x(?P<height>\d+)", line).groupdict()
 
-    for y in range(int(m.group('height'))):
-        for x in range(int(m.group('width'))):
-            if not m.group('id') in canvas[int(m.group('ypos')) + y][int(m.group('xpos')) + x]:
-                canvas[int(m.group('ypos')) + y][int(m.group('xpos')) + x].append(m.group('id'))
+    claimId = m['id']
+    xpos = int(m['xpos'])
+    ypos = int(m['ypos'])
+    width = int(m['width'])
+    height = int(m['height'])
 
-for y in canvas:
-    for x in y:
-        if len(x) == 1:
-            print(x)
+    for row in range(ypos, ypos + height+1):
+        for column in range(xpos, xpos + width+1):
+            if not claimId in canvas[row][column]:
+                canvas[row][column].append(claimId)
+                
+for row in canvas:
+    for column in row:
+        if len(column) == 1:
+            print(column)
+            break
