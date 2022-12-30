95c4d514f8908fc1e812dd560796a4a1fd16472a
Wouter Luberti <zenith@freedom.nl>
2022-12-30 16:27:26 +0100

    Clean up day 3, input day 4

--- 2022/day03b.php
+++ 2022/day03b.php
@@ -6,7 +6,6 @@ $unique = [];
 
 foreach(file($sourceFile) as $line) {
     $data[] = trim($line);
-//    $unique[] = array_values(array_unique(array_intersect(str_split($backpack2), str_split($backpack1))))[0];
 }
 
 function giveThree() {
@@ -20,9 +19,6 @@ foreach (giveThree() as $item) {
     $unique[] = array_values(array_unique(array_intersect(str_split($item[0]), str_split($item[1]), str_split($item[2]))))[0];
 }
 
-//var_dump($unique);
-
-//exit();
 foreach($unique as $val) {
     if (ord($val) >= 65 && ord($val) <= 90 ) {
         $values[] = (ord($val) - 38);
