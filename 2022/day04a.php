d62debd9ad752483e886feb616fad85a6b6f3e5c
Wouter Luberti <zenith@freedom.nl>
2022-12-30 19:32:51 +0100

    Day 4

---
+++ 2022/day04a.php
@@ -0,0 +1,27 @@
+<?php
+
+$sourceFile = 'day04_input.txt';
+$diff = [];
+
+function expandRange($range): array {
+    [$begin, $end] = explode('-', $range);
+    return range($begin, $end);
+}
+
+function rangeInRange($range1, $range2): bool {
+    $range1 = expandRange($range1);
+    $range2 = expandRange($range2);
+
+    if (count($range1) > count($range2)) {
+        return !array_diff($range2, $range1);
+    } else {
+        return !array_diff($range1, $range2);
+    }
+}
+
+foreach(file($sourceFile) as $line) {
+    [$elf1, $elf2] = explode(',', trim($line));
+    $diff[] = rangeInRange($elf1, $elf2);
+}
+
+print(array_sum($diff) . PHP_EOL);
