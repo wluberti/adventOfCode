a7e987f153974477a333ad37a5ec01381af44689
Wouter Luberti <w.luberti@gmail.com>
2024-12-17 00:31:52 +0100

    Day 4 part 2

--- 2024/day04b.php
+++ 2024/day04b.php
@@ -1,63 +1,41 @@
 <?php
-
-// Prepare the input
-$file = file(__DIR__ . '/day04_input.txt');
-foreach ($file as $line) {
-    $data[] = str_split(trim($line));
+$input = file('day04_input.txt', FILE_IGNORE_NEW_LINES);
+$grid = [];
+foreach ($input as $line) {
+    $grid[] = str_split(trim($line));
 }
 
-// Function to check for "MAS" in all directions resembaling an X
-function checkCoordinates(int $row, int $column): int {
-    global $data;
-    $found = 0;
+$rows = count($grid);
+$cols = count($grid[0]);
 
-    // no X-MAS to be found on the edges since A has to be in the middle
-    if ($row === 0 || $row === count($data) - 1 || $column === 0 || $column === count($data[$row]) - 1) {
-        return 0;
-    }
-
-    // All 4 X-directions: [row_offset, col_offset]
-    $directions = [
-        [-1, -1,  1, -1],
-        [-1,  1, -1, -1],
-        [ 1, -1, -1, -1],
-        [-1, -1, -1,  1],
-    ];
-
-    foreach ($directions as [$a, $b, $c, $d]) {
-        $word = '';
-        $word .= $data[$row + $a][$column + $b];
-        $word .= $data[$row][$column]; // A
-        $word .= $data[$row + $c][$column + $d];
-        $word .= $data[$row + -$a][$column + -$b];
-        $word .= $data[$row][$column]; // A
-        $word .= $data[$row + -$c][$column + -$d];
-
-        print($word . PHP_EOL);
-
-        if ( $word === 'SAMSAM' || $word === 'MASMAS' || $word === 'SAMMAS' || $word === 'MASSAM' ) {
-            $found = 1;
+function countXMASCandidates(array $grid, int $rows, int $cols): int {
+    $count = 0;
+    for ($row = 0; $row < $rows - 2; $row++) {
+        for ($col = 0; $col < $cols - 2; $col++) {
+            if ( $grid[$row+1][$col+1] === 'A' && isXMAS($grid, $row, $col)) {
+                $count++;
+            }
         }
     }
-
-    return $found;
+    return $count;
 }
 
-$total = 0;
-
-// Iterate through the grid and check for "XMAS"
-foreach ($data as $line => $letters) {
-    foreach ($letters as $index => $letter) {
-        if ($letter === 'A') { // Optimization: Only start checking if the middle letter is 'A'
-            $total += checkCoordinates($line, $index);
-        }
-    }
+function isXMAS($grid, $row, $col) {
+    // Check the X pattern for two "MAS" or "SAM"
+    return (
+        (     $grid[$row  ][$col  ] === 'M' && $grid[$row+2][$col+2] === 'S' // Top-left to bottom-right MAS
+           && $grid[$row+2][$col  ] === 'M' && $grid[$row  ][$col+2] === 'S' // Bottom-left to top-right MAS
+        )
+        || (   $grid[$row  ][$col  ] === 'S' && $grid[$row+2][$col+2] === 'M' // Top-left to bottom-right SAM
+            && $grid[$row+2][$col  ] === 'S' && $grid[$row  ][$col+2] === 'M' // Bottom-left to top-right SAM
+        )
+        || (   $grid[$row  ][$col  ] === 'M' && $grid[$row+2][$col+2] === 'S' // Top-left to bottom-right MAS
+            && $grid[$row+2][$col  ] === 'S' && $grid[$row  ][$col+2] === 'M' // Bottom-left to top-right SAM
+        )
+        || (   $grid[$row  ][$col  ] === 'S' && $grid[$row+2][$col+2] === 'M' // Top-left to bottom-right SAM
+            && $grid[$row+2][$col  ] === 'M' && $grid[$row  ][$col+2] === 'S' // Bottom-left to top-right MAS
+        )
+    );
 }
 
-print('-----------------------' . PHP_EOL);
-print $total . PHP_EOL;
-
-// 980 is too low
-// 1816 is too low
-// 1949 is incorrect
-// 4020 is too high
+print (countXMASCandidates($grid, $rows, $cols) . PHP_EOL);
